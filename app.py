3f305b92fa2dd1452df320d7858d70d7b93e6d7c
Thomas Lavend'Homme <lavendthomas@outlook.be>
2022-02-23 12:24:45 +0100

    Add exercice 1 -> 3

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask, request
 from github import Github, GithubIntegration
+from matplotlib.style import context
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = 174822
 
 # Read the bot certificate
 with open(
@@ -19,6 +20,46 @@ git_integration = GithubIntegration(
     app_key,
 )
 
+def pr_watch_title(repo, payload):
+    title = payload['pull_request']['title']
+    pr = repo.get_issue(number=payload['pull_request']['number'])
+
+    commit_sha = repo.get_git_ref(f'heads/{payload["pull_request"]["head"]["ref"]}').object.sha
+    print(f"Commit SHA: {commit_sha}")
+
+    if any(w in title.lower() for w in ['wip', 'work in progress', 'do not merge']):
+        # Add pending status
+        repo.get_commit(sha=commit_sha).create_status(
+            state='pending',
+            context="umons-bot/WIP"
+        )
+
+    else:
+        # Check if the pull request has a label called "pending"
+        if not any(label.name == 'pending' for label in pr.labels): # TODO change commit.get_statuses().state == "pending"
+            # Set the status to "success"
+            repo.get_commit(sha=commit_sha).create_status(
+                state='success',
+                context="umons-bot/WIP"
+            )   
+
+
+def pr_watch_comment_status(repo, payload):
+    comment: str = payload["comment"]["body"]
+
+    pr = repo.get_issue(number=payload['pull_request']['number'])
+    commit_sha = repo.get_git_ref(f'heads/{payload["pull_request"]["head"]["ref"]}').object.sha
+
+    if index := comment.find("@umons-bot-tutorial") > 0:
+        rest_of_comment = comment[index+len("@umons-bot-tutorial"):]
+        print(f"Rest of comment: {rest_of_comment}")
+        if "ready for review" in rest_of_comment.lower():
+            # Add a label to the pull request
+            pass # WIP
+
+            
+
+
 def pr_opened_event(repo, payload):
     pr = repo.get_issue(number=payload['pull_request']['number'])
     author = pr.user.login
@@ -30,6 +71,26 @@ def pr_opened_event(repo, payload):
                    f"The repository maintainers will look into it ASAP! :speech_balloon:"
         pr.create_comment(f"{response}")
         pr.add_to_labels("needs review")
+    
+    pr_watch_title(repo, payload)
+
+
+def pr_closed_event(repo, payload):
+    pr = repo.get_issue(number=payload['pull_request']['number'])
+    author = pr.user.login
+
+    # Add a new comment
+    pr.create_comment(f"Thanks for closing this pull request, @{author}! ")
+
+    if payload['pull_request']['merged']:
+        # Delete the branch
+        pr.create_comment(f"This pull request was merged successfully! Deteting the branch.")
+        repo.get_git_ref(f'heads/{payload["pull_request"]["head"]["ref"]}').delete()
+
+    
+def pr_edited_event(repo, payload):
+    pr_watch_title(repo, payload)
+
 
 @app.route("/", methods=['POST'])
 def bot():
@@ -52,7 +113,17 @@ def bot():
     if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
         pr_opened_event(repo, payload)
 
+    # Check if the event is a GitHub pull request closing event
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed':
+        pr_closed_event(repo, payload)
+
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'edited':
+        pr_edited_event(repo, payload)
+
+    if all(k in payload.keys() for k in ['action', 'issue_comment']) and payload['action'] == 'created':
+        pr_watch_comment_status(repo, payload)
+
     return "", 204
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000)
